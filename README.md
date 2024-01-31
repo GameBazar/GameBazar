145e5d13f068f3c7ca2750985168c8dc1f4fb8a2
Andrei Pustovoi <suslicketeam@gmail.com>
2024-02-01 03:54:37 +0800

    feat: update security, create requirements.txt, update readme file

--- README.md
+++ README.md
@@ -6,11 +6,16 @@ Game market place
 
 ## Installation
 
-To get started, you'll need to install the following dependencies:
+1. To get started, you'll need to install the following dependencies:
 
 python3.10
 ```bash
 pip install -r requirements.txt
+```
+
+2. Also you should create `.env` file and create 
+```
+SECRET_KEY=django-token
 ```
 
 ## Credits
--- config/settings.py
+++ config/settings.py
@@ -11,16 +11,19 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sx@bu=xanxisyxy2&pzc$p!n$85guqq#x#sb^5m^l_=$q_o1ec'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Market'
 ]
 
 MIDDLEWARE = [
